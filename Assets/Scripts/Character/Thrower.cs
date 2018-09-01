c8b45fc554dc664098af9ffbddbb69c53287d177
Remy Rey <remy.rey@netlight.com>
2018-09-01 15:16:23 +0200

    Throwing stuff

---
+++ Assets/Scripts/Character/Thrower.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrower : MonoBehaviour {
+
+    public float THROWING_SPEED = 3.0f;
+
+    public GameObject Brick;
+    public GameObject Target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+            Throw();
+	}
+
+    void Throw(){
+        if (this.Brick == null)
+            return;
+
+        Thrown behavior = Brick.GetComponent<Thrown>();
+        if (behavior != null)
+            Destroy(behavior);
+
+        behavior = this.Brick.AddComponent<Thrown>();
+
+        behavior._start = this.gameObject.transform.position;
+        behavior._end = this.Target.transform.position;
+        behavior._speed = THROWING_SPEED;
+
+        this.Brick = null;
+
+    }
+}
