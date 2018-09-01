997c5d657195c4ef0b8200967f1557440f733f9d
Hicks48 <henri.korpela48@gmail.com>
2018-09-01 15:52:39 +0200

    pick up system continued

---
+++ Assets/Scripts/Character/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    private struct Item {
+        public string name;
+        public float weight;
+
+        public Item(string name, float weight) {
+            this.name = name;
+            this.weight = weight;
+        }
+    }
+
+    private IList<Item> items;
+
+    void Start() {
+        this.items = new List<Item>();
+    }
+
+    public void AddItem(string name, float weight) {
+        Item item = new Item(name, weight);
+        this.items.Add(item);
+    }
+}
---
+++ Assets/Scripts/Objects/PickUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    [SerializeField]
+    private string pickupName;
+
+    [SerializeField]
+    private float weight;
+
+    [SerializeField]
+    private Sprite sprite;
+
+    void Start() {
+        this.SetSprite();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Inventory inventory = other.GetComponent<Inventory>();
+
+        if (inventory) {
+            inventory.AddItem(this.pickupName, this.weight);
+            Destroy(this);
+        }
+    }
+
+    private void SetSprite() {
+        SpriteRenderer spriteRenderer = this.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = this.sprite;
+    }
+}
