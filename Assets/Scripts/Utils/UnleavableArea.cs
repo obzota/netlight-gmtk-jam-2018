3118dbf21639756c041b219bd8147de8038439cf
Hicks48 <henri.korpela48@gmail.com>
2018-09-02 20:22:04 +0200

    Player cant leave the play area

---
+++ Assets/Scripts/Utils/UnleavableArea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnleavableArea : MonoBehaviour {
+
+    private BoxCollider boxCollider;
+
+	void Start () {
+        this.boxCollider = this.GetComponent<BoxCollider>();
+	}
+
+    void OnTriggerExit(Collider other) {
+        //Vector3 direction = this.DirectionFromObjectToCenter(other.transform.position);
+        other.transform.position = this.boxCollider.ClosestPoint(other.transform.position);
+    }
+
+    private Vector3 DirectionFromObjectToCenter(Vector3 objectPosition) {
+        Vector3 towardsCenter = this.transform.position - objectPosition;
+        towardsCenter.Normalize();
+        return towardsCenter;
+    }
+}
