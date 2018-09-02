e0be57fbc42932c7dba4ba03b29662559537ac2b
Hicks48 <henri.korpela48@gmail.com>
2018-09-02 16:21:10 +0200

    Added animations to the ball

---
+++ Assets/Scripts/Objects/PhysicsMovementProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsMovementProvider : MonoBehaviour, IMovementProvider {
+
+    private Rigidbody rb;
+
+    void Start() {
+        this.rb = this.GetComponent<Rigidbody>();
+    }
+
+    Vector3 IMovementProvider.GetMovement() {
+        return this.rb.velocity;
+    }
+}
