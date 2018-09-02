c9fc6f627efb6633031559fe66dccb93b03cd2ca
Hicks48 <henri.korpela48@gmail.com>
2018-09-02 19:37:52 +0200

    Larger colliders for pickups

--- Assets/Scripts/Utils/SphereColliderResizer.cs
+++ Assets/Scripts/Utils/SphereColliderResizer.cs
@@ -8,6 +8,6 @@ public class SphereColliderResizer : MonoBehaviour {
         SphereCollider sphereCollider = this.GetComponent<SphereCollider>();
         Bounds sprite3DBounds = this.GetComponentInChildren<Sprite3D>().GetBounds();
         float largestSide = Mathf.Max(sprite3DBounds.size.x, sprite3DBounds.size.y, sprite3DBounds.size.z);
-        sphereCollider.radius = largestSide / 2.0f;
+        sphereCollider.radius = largestSide;
     }
 }
