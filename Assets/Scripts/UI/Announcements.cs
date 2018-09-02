9805218bf10710127e06f180f458c91785fb1e24
Hicks48 <henri.korpela48@gmail.com>
2018-09-02 12:26:17 +0200

    Added game ui

---
+++ Assets/Scripts/UI/Announcements.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Announcements : MonoBehaviour {
+
+    [SerializeField]
+    private Text announcementText;
+
+    private string currentAnnouncement = null;
+    private float currentAnnouncementDuration;
+    private float currentlyOnScreenTime;
+
+	void Start () {
+        this.announcementText.text = "";
+	}
+
+    void Update() {
+
+        if (this.currentAnnouncement != null) {
+            this.UpdateAnnouncement();
+        }
+    }
+
+    public void MakeAnnouncement(string text, float duration) {
+        this.currentAnnouncement = text;
+        this.currentAnnouncementDuration = duration;
+        this.currentlyOnScreenTime = 0.0f;
+
+        this.announcementText.text = text;
+    }
+
+    private void UpdateAnnouncement() {
+        this.currentlyOnScreenTime += Time.deltaTime;
+
+        if (this.currentlyOnScreenTime >= this.currentAnnouncementDuration) {
+            this.ResetAnnouncement();
+        }
+    }
+
+    private void ResetAnnouncement() {
+        this.announcementText.text = "";
+        this.currentAnnouncement = null;
+    }
+}
---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    [SerializeField]
+    private Text menuText;
+
+    public bool Visible {
+        get {
+            return this.menuText.enabled;
+        }
+
+        set {
+            this.menuText.enabled = value;
+        }
+    }
+}
