2bfaf60c91c0fa0c5ce4d324d77806b70c6fb4c2
Remy Rey <remy.rey@netlight.com>
2018-09-02 11:38:20 +0200

    simple gamestate implemented

---
+++ Assets/Scripts/Systems/Pause.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour
+{
+    
+    private float duration;
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GoOn())
+        {
+            duration -= Time.deltaTime;
+        }
+    }
+
+    public bool GoOn(){
+        return duration > 0;
+    }
+
+    public void Begin(float TimeInSeconds){
+        duration = TimeInSeconds;
+    }
+}
