712a29c724dd883232fb87df9e801556775fbf4f
Remy Rey <remy.rey@netlight.com>
2018-09-02 19:44:24 +0200

    scoring behavior when in contact with triggers

---
+++ Assets/Scripts/Objects/ScoreGoal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreGoal : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject RedGoalTrigger;
+    [SerializeField]
+    private GameObject BluGoalTrigger;
+    [SerializeField]
+    private Game controler;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == RedGoalTrigger || other.gameObject == BluGoalTrigger)
+            controler.Score(other.gameObject);
+    }
+}
